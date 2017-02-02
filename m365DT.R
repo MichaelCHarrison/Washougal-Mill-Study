3701522d02e306a99fd2830255e87b7685ce9963
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-02-01 22:43:42 -0800

    Deleted obsolete functions

--- m365DT.R
+++
@@ -1,8 +0,0 @@
-m365DT <- function(){
-        m365_df <- read.csv("~/Desktop/Pendleton/CSVs/M365 for Micheal's Project 161228.csv")
-        
-        library(data.table)
-        m365_dt <- data.table(m365_df)
-        
-        m365_dt
-}
