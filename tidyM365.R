a4d6dc70f4dacb177eca9df77c273e5875b3f857
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-02-01 22:21:26 -0800

    Tidy-er commit

--- tidyM365.R
+++ tidyM365.R
@@ -13,35 +13,9 @@ tidyM365 <- function(){
         m365_tbl$range <- gsub("(^[^1-9]*)(\\d+)((-|[A-Z]).+$)", "\\2", m365_tbl$millstyle)
 
         m365_tbl %>%
-               select(range, order, duedate, date, yds, loom)
+               select(range, order, duedate, date, yds, loom) %>%
+                print()
+               
+}
 
-}       
-        
-        # range_tbl <- tbl_df(range_df)
-        # range_tbl$RANGE <- as.numeric(as.character(range_tbl$RANGE))
-        # tolower(names(range_tbl))
-        # range_tbl
-                # select()
-                # group_by(range_tbl$RANGE)%>%
-
-
-        
-        # library(data.table)
-        # range_dt <- data.table(range_df)
-        # range_dt$RANGE <- as.numeric(as.character(range_dt$RANGE))
-        # range_dt <- range_dt[, .(TotalMPX = sum(MPX),
-        #                          TotalStops = sum(Combined.Stops),
-        #                          PicksPerStop = sum(MPX)/sum(Combined.Stops)),
-        #                      by = range_dt$RANGE]
-        # 
-        # pps_order <- na.omit(range_dt[order(-PicksPerStop)])
-        # totalMPX_order <- na.omit(range_dt[order(-TotalMPX)])
-        # 
-        # top_perf_pps <- head(pps_order[which(pps_order$TotalMPX > (summary(pps_order$TotalMPX))[2])])
-        # bottom_perf_pps <- tail(pps_order[which(pps_order$TotalMPX > (summary(pps_order$TotalMPX))[2])])
-        # top_perf_MPX <- head(totalMPX_order[which(totalMPX_order$TotalMPX > (summary(totalMPX_order$TotalMPX))[2])])
-        # bottom_perf_MPX <- tail(totalMPX_order[which(totalMPX_order$TotalMPX > (summary(totalMPX_order$TotalMPX))[2])])
-        # 
-        # list(top5pps = top_perf_pps, bottom5pps = bottom_perf_pps, 
-        #      top5mpx = top_perf_MPX, bottom5mpx = bottom_perf_MPX)
 
--- tidyYarns.R
+++ tidyYarns.R
@@ -11,6 +11,7 @@ tidyYarns <- function(){
         yarn_tbl <- tbl_df(yarn_df)
         yarn_tbl <- yarn_tbl %>%
                 select(yarn, millstyle, pounds, type)
+                #mutate a new variable of number of yarns that go into a millstyle
         
         yarn_tbl
 }
