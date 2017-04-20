8fb9d225a9c903bdf65dc3b88d3863b04e692576
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-04-19 17:54:29 -0700

    Experimenting on how to merge stops and m365

--- tidyYarns.R
+++ tidyYarns.R
@@ -3,10 +3,9 @@ tidyYarns <- function(){
         twist_df <-read.csv("~/Desktop/Pendleton/CSVs/Stops-TwistYarns.csv")
         dyed_df <-read.csv("~/Desktop/Pendleton/CSVs/Stops-DyedYarns.csv")
         spun_df <- read.csv("~/Desktop/Pendleton/CSVs/Stops-SpunYarns.csv")
-        #read all csvs containing string "Yarns" into list, then bind?
-        
+
         library(dplyr)
-        yarn_df <- suppressWarnings(bind_rows(pur_df, twist_df, dyed_df, spun_df))
+        yarn_df <- bind_rows(pur_df, twist_df, dyed_df, spun_df)
         names(yarn_df) <- tolower(names(yarn_df))
         names(yarn_df) <- sub("\\.","",names(yarn_df))
         yarn_df$millstyle <- as.factor(yarn_df$millstyle)
