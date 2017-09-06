355667466bd44c48a10f55da4dd1b233889e0507
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-09-06 14:49:32 -0700

    added function to return number of NAs in each column of df

--- NAcheck.R
+++ NAcheck.R
@@ -1,5 +1,15 @@
 #Function to check for NAs in dataframes
 
-NAcheck <- function(data){
+checkNA <- function(data){
         sapply(data, function(x) sum(is.na(x)))
+}
+
+# for NAs in tidy_m365$duedate, determine whether to remove rows, 
+# disregard value, or remove entirely
+#
+# for NAs in tidy_m365$order, determine whether this affects the ability to compress the 
+# data in compressedM365.R and if that has an relevance on how 
+
+returnNARows <- function(data){
+        data[rowSums(is.na(data)) > 0,]
 }
