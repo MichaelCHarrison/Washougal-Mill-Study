b04ad8f241d9de8717a022ef9a2d013f7bf0adab
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-09-07 13:16:11 -0700

    Removed datelastused field

--- tidyBom.R
+++ tidyBom.R
@@ -6,12 +6,10 @@ tidyBom <- function(){
         names(bom_tbl) <- tolower(names(bom_tbl))
         names(bom_tbl) <- sub("\\.", "", names(bom_tbl))
         colnames(bom_tbl)[1] <- "millstyle"
-        #millstyle set to factor to get intersect() to work; 09/06
         bom_tbl$millstyle <- trimws(bom_tbl$millstyle)
         bom_tbl$millstyle <- as.factor(bom_tbl$millstyle)
-        bom_tbl$datelastused <- mdy(bom_tbl$datelastused)
         
         bom_tbl %>%
-                select(millstyle, yarn, lbsperpiece, datelastused)
-        return(bom_tbl)        
+                select(millstyle, yarn, lbsperpiece) 
+
 }
