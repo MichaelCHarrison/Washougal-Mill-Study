c82c52e246b2c7636e5639962ed52f1c61ba5480
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-09-06 21:37:49 -0700

    Returns desired table; need to verify values

--- joinedM365Stops.R
+++ joinedM365Stops.R
@@ -1,7 +1,6 @@
 joinedM365Stops <- function(){
         library(sqldf); library(dplyr)
-        setwd()
-        source(tidyM365.R); source(tidyStops())
+        source("tidyM365.R"); source("tidyStops.R")
         
         tidy_m365 <- tidyM365()
         tidy_Stops <- tidyStops()
@@ -18,7 +17,10 @@ joinedM365Stops <- function(){
                           totalstops = sum(combinedstops)) %>%
                 select(date, loom, range, millstyle, avgppm, 
                        totalpx, totalyards, totalstops) %>%
-                arrange(desc(date)) 
+                arrange(-desc(date), loom) 
         
         return(tidySQLtbl)
 }
+
+# source sqldf package:
+# https://stackoverflow.com/questions/1299871/how-to-join-merge-data-frames-inner-outer-left-right
