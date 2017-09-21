ec6e857884da19b3ae5c1348d85eda2e74001b6b
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-09-21 15:55:10 -0700

    Changed partitions, set algorithm suite

--- predictStop.R
+++ predictStop.R
@@ -13,17 +13,17 @@ predictStops <- function(){
         
         # Partitions data to create validation set for model
         inVal <- createDataPartition(data$totalstops,
-                                     p = .8,
+                                     p = .85,
                                      list = FALSE)
         validation <- data[-inVal,]
-        dataset <- data[inVal,]
+        traing_test <- data[inVal,]
         
         # Create training/testing sets
         inTrain <- createDataPartition(dataset$totalstops, 
-                                       p = .8,
+                                       p = .85,
                                        list = FALSE)
-        training <- dataset[-inTrain,]
-        testing <- dataset[inTrain,]
+        training <- traing_test[inTrain,]
+        testing <- traing_test[-inTrain,]
         
         
         # Set training control parameters for repeated cross validation and evaluation metric
@@ -33,42 +33,77 @@ predictStops <- function(){
         metric <- "RMSE"
         
         
+        #Models
+        
+        #Linear Model
+        set.seed(seed)
+        fit.lm <- train(totalstops~.,
+                        data = training,
+                        method = "lm",
+                        metric = metric,
+                        trControl = trainControl)
         
         
+        #Generalized Linear Model
+        set.seed(seed)
+        fit.glm <- train(totalstops~.,
+                         data = training,
+                         method = "glm",
+                         metric = metric,
+                         trControl = trainControl)
         
-        # LM
+        #GLMNET
         set.seed(seed)
-        fit.lm <- train(totalstops~., data=training, method="lm", 
-                        metric=metric, 
-                        trControl=trainControl)
-        # GLM
-        set.seed(seed)
-        fit.glm <- train(totalstops~., data=training, method="glm", 
-                         metric=metric, 
-                         trControl=trainControl)
-        # GLMNET
-        set.seed(seed)
-        fit.glmnet <- train(totalstops~., data=training, method="glmnet", metric=metric, 
-                            trControl=trainControl)
+        fit.glmnet <- train(totalstops~.,
+                            data = training,
+                            method = "glmnet",
+                            metric = metric,
+                            trControl = trainControl)
+        
         # SVM
         set.seed(seed)
-        fit.svm <- train(totalstops~., data=training, method="svmRadial", metric=metric, 
-                         trControl=trainControl)
+        fit.svm <- train(totalstops~.,
+                         data = training,
+                         method = "svmRadial",
+                         metric = metric,
+                         trControl = trainControl)
+        
         # CART
         set.seed(seed)
         grid <- expand.grid(.cp=c(0, 0.05, 0.1))
-        fit.cart <- train(totalstops~., data=training, method="rpart", metric=metric, 
-                          tuneGrid=grid, 
-                          trControl=trainControl)
+        fit.cart <- train(totalstops~.,
+                          data = training,
+                          method = "rpart",
+                          metric = metric,
+                          tuneGrid=grid,
+                          trControl = trainControl)
+                
+
         # KNN
         set.seed(seed)
-        fit.knn <- train(totalstops~., data=training, method="knn", 
-                         metric=metric, 
-                         trControl=trainControl)
+        fit.knn <- train(totalstops~.,
+                         data = training,
+                         method = "knn",
+                         metric = metric,
+                         trControl = trainControl)
         
+        # Random Forest
+        set.seed(seed)
+        fit.rf <- train(totalstops~.,
+                        data = training,
+                        method = "rf",
+                        metric = metric,
+                        trControl = trainControl)
+        
+         
         # Results
-        results <- resamples(list(LM=fit.lm, GLM=fit.glm, GLMNET=fit.glmnet, SVM=fit.svm,
-                                  CART=fit.cart, KNN=fit.knn))
+        results <- resamples(list(LM=fit.lm, 
+                                  GLM=fit.glm, 
+                                  GLMNET=fit.glmnet, 
+                                  SVM=fit.svm,
+                                  CART=fit.cart, 
+                                  KNN=fit.knn,
+                                  RF=fit.rf))
         
         
 }
