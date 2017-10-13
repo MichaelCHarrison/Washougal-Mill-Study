81dd675fa0da23077c7fae6ae2d674a8f61ffee2
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-10-12 17:07:56 -0700

    Comment and description updated

--- joinedM365Stops.R
+++ joinedM365Stops.R
@@ -1,33 +1,30 @@
-# This function joins the returned tables from the tidyM365.R and tidyStops.R functions. 
-# After the merge, data is grouped by date, range, millstyle, and loom to engineer 4 features 
-# and the prediction target based on the production data from tidyM365.R: 
+# joinedm365Stops.R is a function that joins the tables returned from the tidyM365.R and 
+# tidyStops.R. The joined table is then grouped by date, range, millstyle, and loom to 
+# engineer 4 features and the prediction target based on the production data from tidyM365.R: 
 #
-#       avgppm = average picks per minute
-#       totalpx = total picks
-#       totalyards = total yards
-#       yardsperpx = yards per pick
-#       totalstops = total stops (prediction target)
-#
-# The table returned by this function is to be joined with the table returned by 
-# joinedBomYarns.R on the millstyle feature 
+#       avgppm - average picks per minute
+#       totalpx - total picks
+#       totalyards - total yards
+#       yardsperpx - yards per pick
+#       totalstops - total stops (prediction target)
+
 
 joinedM365Stops <- function(){
+        # Load packages and functions
         library(sqldf); library(dplyr)
         source("tidyM365.R"); source("tidyStops.R")
         
+        # Set returned tables to local variable
         tidy_m365 <- tidyM365()
         tidy_Stops <- tidyStops()
         
-        # replaced SQL query with data.table merge to keep consistency across scripts
-        # sqltbl <- sqldf("SELECT date, loom, range, millstyle, ppm, mpx, yds, combinedstops 
-        #                  FROM tidy_m365
-        #                  JOIN tidy_Stops USING(date, loom, range)")
-
+        # Merge the two tables
         tidy_M365Stops <- merge(tidy_m365, 
                                tidy_Stops, 
                                by = c("date", "loom", "range"))
         
-        tidy_M365Stops <- tidy_M365Stops %>%
+        # Group by fields and calculate new fields
+        joined_M365Stops <- tidy_M365Stops %>%
                 group_by(date, range, millstyle, loom) %>%
                 summarize(avgppm = mean(ppm),
                           totalpx = sum(mpx),
@@ -38,8 +35,5 @@ joinedM365Stops <- function(){
                        totalpx, totalyards, yardsperpx, totalstops) %>%
                 arrange(-desc(date), loom) 
         
-        return(tidy_M365Stops)
+        return(joined_M365Stops)
 }
-
-# source sqldf package:
-# https://stackoverflow.com/questions/1299871/how-to-join-merge-data-frames-inner-outer-left-right
